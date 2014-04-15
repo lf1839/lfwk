5719de983a334a5ba2c90f29fe8d6888ba2c503b
Ben Nizette <bennizette@gmail.com>
2014-04-15 17:27:21 +0800

    SCSC: Change to octa frame type with user extensions

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -5,7 +5,13 @@
 // If you used to define your CONFIG_APM_HARDWARE setting here, it is no longer
 // valid! You should switch to using a HAL_BOARD flag in your local config.mk.
 
-//#define FRAME_CONFIG QUAD_FRAME
+#define FRAME_CONFIG OCTA_FRAME
+//#define HIL_MODE              HIL_MODE_SENSORS    // build for hardware-in-the-loop simulation
+//#define LOGGING_ENABLED       DISABLED            // disable logging to save code space
+//#define DMP_ENABLED           ENABLED             // use MPU6000's DMP instead of DCM for attitude estimation
+//#define SECONDARY_DMP_ENABLED ENABLED             // allows running DMP in parallel with DCM for testing purposes
+
+
 /*
  *  options:
  *  QUAD_FRAME
@@ -42,10 +48,10 @@
 
 // User Hooks : For User Developed code that you wish to run
 // Put your variable definitions into the UserVariables.h file (or another file name and then change the #define below).
-//#define USERHOOK_VARIABLES "UserVariables.h"
+#define USERHOOK_VARIABLES "UserVariables.h"
 // Put your custom code into the UserCode.pde with function names matching those listed below and ensure the appropriate #define below is uncommented below
-//#define USERHOOK_INIT userhook_init();                      // for code to be run once at startup
-//#define USERHOOK_FASTLOOP userhook_FastLoop();            // for code to be run at 100hz
+#define USERHOOK_INIT userhook_init();                      // for code to be run once at startup
+#define USERHOOK_FASTLOOP userhook_FastLoop();            // for code to be run at 100hz
 //#define USERHOOK_50HZLOOP userhook_50Hz();                  // for code to be run at 50hz
 //#define USERHOOK_MEDIUMLOOP userhook_MediumLoop();        // for code to be run at 10hz
 //#define USERHOOK_SLOWLOOP userhook_SlowLoop();            // for code to be run at 3.3hz
