b06e0bb679e24d0cf6c31f0e16f2a60391772fc0
Ben Nizette <bennizette@gmail.com>
2014-06-23 09:27:34 +0800

    SCSC: Wire in gas sensor variables

--- ArduCopter/UserVariables.h
+++ ArduCopter/UserVariables.h
@@ -9,13 +9,25 @@
 int strobe_cnt;
 int startup_cnt;
 
-#if WII_CAMERA == 1
-WiiCamera           ircam;
-int                 WiiRange=0;
-int                 WiiRotation=0;
-int                 WiiDisplacementX=0;
-int                 WiiDisplacementY=0;
-#endif  // WII_CAMERA
+unsigned int O2;
+unsigned int CO2;
+
+union b16 {
+    int N;
+    unsigned char B[2];
+};
+
+union b16 CO;
+union b16 NO;
+union b16 NO2;
+union b16 SO2;
+union b16 H2S;    
+union b16 COC;
+union b16 NOC;
+union b16 NO2C;
+union b16 SO2C;
+union b16 H2SC;
+    
 
 #endif  // USERHOOK_VARIABLES
 
